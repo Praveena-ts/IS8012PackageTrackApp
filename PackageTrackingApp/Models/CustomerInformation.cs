f31de5462218f8fedf947943476d018f8e84b65e
Praveena-ts <77545695+Praveena-ts@users.noreply.github.com>
2021-07-28 15:04:34 -0400

    Initial code commit

---
+++ PackageTrackingApp/Models/CustomerInformation.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PackageTrackingApp.Models
+{
+    public class CustomerInformation
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public DateTime DateOfBirth { get; set; }
+        public string Address { get; set; }
+        public int PhoneNumber { get; set; }
+        public bool? AlreadyCustomer { get; set; }
+       
+    }
+}
---
+++ PackageTrackingApp/Models/ShippingInfo.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PackageTrackingApp.Models
+{
+    public class ShippingInfo
+    {
+        public int Id { get; set; }
+        public int PackageNumber { get; set; }
+        public string PackageStatus { get; set; }
+
+
+
+    }
+}
